987dbb9e62c8884ed57552f43f8ccfe373193155
Helder Balbino <helderbalbino@googlemail.com>
2025-06-03 22:01:25 +0100

    feat: update Navbar component to display logo image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 import logo from '../assets/images/logo.png';
 
 const Navbar = () => {
-	return <div>Bartenders' Hub</div>;
+	return (
+		<div>
+			<img className='h-28 w-auto' src={logo} alt='' />
+		</div>
+	);
 };
 
 export default Navbar;
