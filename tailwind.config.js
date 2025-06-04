00b12ac8b36702058d4143a343d5270fc6704320
Helder Balbino <helderbalbino@googlemail.com>
2025-06-04 22:09:00 +0100

    refactor: update Tailwind CSS configuration for improved content paths and theme settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,29 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+module.exports = {
+	content: [
+		'./src/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/sections/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
 	theme: {
+		screens: {
+			sm: '375px',
+			md: '768px',
+			lg: '1200px',
+		},
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: '1rem',
+				md: '2rem',
+			},
+		},
 		extend: {
 			fontFamily: {
-				sans: ['Roboto', 'sans-serif'],
-			},
-			gridTemplateColumns: {
-				'70-30': '70% 28%',
-			},
-			colors: {
-				gold: '#FFD701',
-			},
-			borderColor: {
-				gold: '#FFD700',
+				sans: 'var(--font-sans)',
+				serif: 'var(--font-serif)',
 			},
 		},
 	},
