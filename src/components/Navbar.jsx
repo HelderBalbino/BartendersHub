c472243e8c623666dbbdb525d47bd1c38017e633
Helder Balbino <helderbalbino@googlemail.com>
2025-06-04 17:06:07 +0100

    feat: restructure Navbar component layout and styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,22 @@ import logo from '../assets/images/logo.png';
 const Navbar = () => {
 	return (
 		<nav className='bg-black border border-gold'>
-			<img className='h-28 w-auto' src={logo} alt='' />
+			<div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
+				<div className='flex h-20 items-center justify-between'>
+					<div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
+						<a
+							className='flex flex-shrink-0 items-center mr-4'
+							href='/index.html'
+						>
+							<img
+								className='block h-16 w-auto'
+								src={logo}
+								alt='Logo'
+							/>
+						</a>
+					</div>
+				</div>
+			</div>
 		</nav>
 	);
 };
