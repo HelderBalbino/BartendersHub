0e8fad1f4197a3d3b9d2ee8b88aa76f022159f5a
Helder Balbino <helderbalbino@googlemail.com>
2025-06-17 23:50:40 +0100

    feat: add Hero component to App and render it in the main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 const App = () => {
 	return (
 		<>
 			<Navbar />
+			<Hero />
+
 			{/* <Footer /> */}
 		</>
 	);
