14ad875e7bbb29386c4ecfbdc2a5a2252b184042
Helder Balbino <helderbalbino@googlemail.com>
2025-06-20 01:15:27 +0100

    fix: remove unused component imports from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,6 @@ import {
 	createRoutesFromElements,
 	RouterProvider,
 } from 'react-router-dom';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Content from './components/Content';
-import { HiH1 } from 'react-icons/hi2';
 
 const router = createBrowserRouter(
 	createRoutesFromElements(<Route index element={<h1>My App</h1>} />),
