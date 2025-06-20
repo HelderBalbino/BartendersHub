2135c6f66121e90dd7843cfe9c82ceb0b2674490
Helder Balbino <helderbalbino@googlemail.com>
2025-06-20 01:12:27 +0100

    fix: simplify App component by removing unused imports and returning RouterProvider

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,14 @@ import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Content from './components/Content';
+import { HiH1 } from 'react-icons/hi2';
+
+const router = createBrowserRouter(
+	createRoutesFromElements(<Route index element={<h1>My App</h1>} />),
+);
 
 const App = () => {
-	return (
-		<>
-			<Navbar />
-			<Hero />
-			<Content />
-			<Footer />
-		</>
-	);
+	return <RouterProvider router={router} />;
 };
 
 export default App;
