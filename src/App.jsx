e0bfdde890b38f73d8f78d372e0ff84fcd83e7ae
Helder Balbino <helderbalbino@googlemail.com>
2025-06-27 14:16:51 +0100

    feat: add CommunityPage route to the application

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import {
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import CocktailsPage from './pages/CocktailsPage';
+import CommunityPage from './pages/CommunityPage';
 import AboutPage from './components/AboutContent';
 
 const router = createBrowserRouter(
@@ -14,6 +15,7 @@ const router = createBrowserRouter(
 		<Route path='/' element={<MainLayout />}>
 			<Route index element={<HomePage />} />
 			<Route path='/cocktails' element={<CocktailsPage />} />
+			<Route path='/community' element={<CommunityPage />} />
 			<Route path='/about' element={<AboutPage />} />
 		</Route>,
 	),
