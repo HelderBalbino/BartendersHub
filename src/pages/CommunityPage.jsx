3d4e17291df4aa9d873ebe1f1049e1c0677d39d9
Helder Balbino <helderbalbino@googlemail.com>
2025-06-27 14:17:45 +0100

    feat: create CommunityPage component to integrate CommunitySection

---
+++ src/pages/CommunityPage.jsx
@@ -0,0 +1,11 @@
+import CommunitySection from '../components/CommunitySection';
+
+const CommunityPage = () => {
+	return (
+		<div className='min-h-screen bg-black'>
+			<CommunitySection />
+		</div>
+	);
+};
+
+export default CommunityPage;
