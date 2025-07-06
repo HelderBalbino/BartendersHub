3c43446755a16bd238a131cfaca4906babc0e642
Helder Balbino <helderbalbino@googlemail.com>
2025-07-06 23:49:01 +0100

    feat: create AddCocktailPage component to integrate cocktail section

---
+++ src/pages/AddCocktailPage.jsx
@@ -0,0 +1,11 @@
+import AddCocktailSection from '../components/AddCocktailSection';
+
+const AddCocktailPage = () => {
+	return (
+		<>
+			<AddCocktailSection />
+		</>
+	);
+};
+
+export default AddCocktailPage;
