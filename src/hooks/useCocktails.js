f3dd8b5ef3e545fd6355941fe34f2a7f451582b0
Helder Balbino <helderbalbino@googlemail.com>
2025-07-10 14:41:46 +0100

    feat: Update react-query import to @tanstack/react-query

--- src/hooks/useCocktails.js
+++ src/hooks/useCocktails.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useQuery, useMutation, useQueryClient } from 'react-query';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import apiService from '../services/api';
 import { toast } from 'react-hot-toast';
 
