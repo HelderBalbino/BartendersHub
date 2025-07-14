65fb890af96716452235ec9cc53e1aa384c7458b
Helder Balbino <helderbalbino@googlemail.com>
2025-07-14 15:00:13 +0100

    refactor: Simplify MongoDB connection by removing unnecessary options

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,11 +1,9 @@
 import mongoose from 'mongoose';
+import process from 'process';
 
 const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect(process.env.MONGODB_URI, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
+		const conn = await mongoose.connect(process.env.MONGODB_URI);
 
 		console.log(`MongoDB Connected: ${conn.connection.host}`);
 	} catch (error) {
