c0c2be222c138b569fa1eeff9697a140a3d6816f
Helder Balbino <helderbalbino@googlemail.com>
2025-07-21 00:42:10 +0100

    feat: add ImageAndTagsForm component for cocktail image and tag input

---
+++ src/components/AddCocktailSection/CocktailForm/ImageAndTagsForm.jsx
@@ -0,0 +1,46 @@
+import PropTypes from 'prop-types';
+import ImageUpload from '../../ui/Forms/ImageUpload';
+import TagInput from '../../ui/Forms/TagInput';
+import FormSection from '../../ui/Forms/FormSection';
+
+const ImageAndTagsForm = ({
+	imagePreview,
+	onImageChange,
+	onImageRemove,
+	tags,
+	onTagsChange,
+}) => {
+	return (
+		<FormSection title='Image & Tags'>
+			<ImageUpload
+				preview={imagePreview}
+				onChange={onImageChange}
+				onRemove={onImageRemove}
+				placeholder='Cocktail Image'
+			/>
+
+			<div>
+				<label className='block text-yellow-400 text-sm font-light tracking-wide uppercase mb-3'>
+					Tags
+				</label>
+				<TagInput
+					items={tags}
+					onChange={onTagsChange}
+					placeholder='Tag'
+					fieldName='tags'
+					minItems={0}
+				/>
+			</div>
+		</FormSection>
+	);
+};
+
+ImageAndTagsForm.propTypes = {
+	imagePreview: PropTypes.string,
+	onImageChange: PropTypes.func.isRequired,
+	onImageRemove: PropTypes.func.isRequired,
+	tags: PropTypes.array.isRequired,
+	onTagsChange: PropTypes.func.isRequired,
+};
+
+export default ImageAndTagsForm;
