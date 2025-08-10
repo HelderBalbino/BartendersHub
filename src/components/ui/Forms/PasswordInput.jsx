3ff3364ebf73947c2c81b9b78206517ed710ef7e
Helder Balbino <helderbalbino@googlemail.com>
2025-08-10 14:25:06 +0100

    fix: Improve formatting of aria-label for password visibility toggle

--- src/components/ui/Forms/PasswordInput.jsx
+++ src/components/ui/Forms/PasswordInput.jsx
@@ -50,7 +50,9 @@ const PasswordInput = ({
 					type='button'
 					onClick={togglePasswordVisibility}
 					className='absolute right-3 top-1/2 transform -translate-y-1/2 text-yellow-400/70 hover:text-yellow-400 transition-all duration-200 focus:outline-none focus:text-yellow-400 focus:ring-2 focus:ring-yellow-400/50 focus:ring-offset-2 focus:ring-offset-black/20 rounded p-1'
-					aria-label={showPassword ? 'Hide password' : 'Show password'}
+					aria-label={
+						showPassword ? 'Hide password' : 'Show password'
+					}
 					tabIndex={0}
 				>
 					{showPassword ? (
