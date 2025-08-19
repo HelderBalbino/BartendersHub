96dd6a1f41239ba4ed1f5151c31eac77a2816abb
Helder Balbino <helderbalbino@googlemail.com>
2025-08-20 00:54:49 +0100

    test: Update Jest configuration to include coverage settings and thresholds

--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -3,4 +3,16 @@ module.exports = {
 	transform: {},
 	globalSetup: './tests/setup/globalSetup.js',
 	globalTeardown: './tests/setup/globalTeardown.js',
+	collectCoverage: true,
+	coverageDirectory: 'coverage',
+	coverageReporters: ['text', 'lcov'],
+	coveragePathIgnorePatterns: ['/node_modules/', '/tests/'],
+	coverageThreshold: {
+		global: {
+			branches: 35,
+			functions: 45,
+			lines: 45,
+			statements: 45,
+		},
+	},
 };
