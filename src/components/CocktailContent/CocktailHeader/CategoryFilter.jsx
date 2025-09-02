7d346800467ccdcfc037be05e1f76dd624789565
Helder Balbino <helderbalbino@googlemail.com>
2025-09-02 14:28:23 +0100

    refactor: Simplify CategoryFilter component by removing PropTypes and redundant wrapper

--- src/components/CocktailContent/CocktailHeader/CategoryFilter.jsx
+++ src/components/CocktailContent/CocktailHeader/CategoryFilter.jsx
@@ -1,20 +1,11 @@
-import PropTypes from 'prop-types';
+// DEPRECATED: This wrapper was redundant. Keep export temporarily to avoid breaking imports.
+// Switch existing imports to use FilterTabs directly then remove this file.
 import FilterTabs from '../../ui/Navigation/FilterTabs';
-
-const CategoryFilter = ({ categories, activeCategory, onCategoryChange }) => {
-	return (
-		<FilterTabs
-			filters={categories}
-			activeFilter={activeCategory}
-			onChange={onCategoryChange}
-		/>
-	);
-};
-
-CategoryFilter.propTypes = {
-	categories: PropTypes.array.isRequired,
-	activeCategory: PropTypes.string.isRequired,
-	onCategoryChange: PropTypes.func.isRequired,
-};
-
+const CategoryFilter = (props) => (
+	<FilterTabs
+		filters={props.categories}
+		activeFilter={props.activeCategory}
+		onChange={props.onCategoryChange}
+	/>
+);
 export default CategoryFilter;
